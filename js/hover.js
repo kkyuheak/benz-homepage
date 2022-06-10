ce22e20dddfa7138ddf462f3898b5746dfcba43a
gyuheak@gmail.com <gyuheak@gmail.com>
2022-06-11 01:26:59 +0900

    Project

--- js/hover.js
+++ js/hover.js
@@ -13,7 +13,12 @@ const btnClick2 = function () {
   hiddenBoxShow2.style.display = "block";
 };
 
-const imgClick2 = function () {
-  const imgClose2 = document.getElementById("ts_fea-hidden-2");
-  imgClose2.style.display = "none";
+const btnClick3 = function () {
+  const hiddenBoxShow3 = document.getElementById("ts_fea-hidden-3");
+  hiddenBoxShow3.style.display = "block";
+};
+
+const imgClick3 = function () {
+  const imgClose3 = document.getElementById("ts_fea-hidden-3");
+  imgClose3.style.display = "none";
 };
