43ff9de53c3ad00e7060710fbbfcd2f465490c39
gyuheak@gmail.com <gyuheak@gmail.com>
2022-06-14 00:23:49 +0900

    Project

--- js/hover.js
+++ js/hover.js
@@ -13,6 +13,11 @@ const btnClick2 = function () {
   hiddenBoxShow2.style.display = "block";
 };
 
+const imgClick2 = function () {
+  const imgClose2 = document.getElementById("ts_fea-hidden-2");
+  imgClose2.style.display = "none";
+};
+
 const btnClick3 = function () {
   const hiddenBoxShow3 = document.getElementById("ts_fea-hidden-3");
   hiddenBoxShow3.style.display = "block";
@@ -21,4 +26,36 @@ const btnClick3 = function () {
 const imgClick3 = function () {
   const imgClose3 = document.getElementById("ts_fea-hidden-3");
   imgClose3.style.display = "none";
+};
+
+const btnClick4 = function () {
+  const hiddenBoxShow4 = document.getElementById("ts_fea-hidden-4");
+  hiddenBoxShow4.style.display = "block";
+};
+
+const imgClick4 = function () {
+  const imgClose4 = document.getElementById("ts_fea-hidden-4");
+  imgClose4.style.display = "none";
+};
+
+const iconNone = function () {
+  const NoIcon = document.querySelector(".third-screen-icon");
+  NoIcon.style.display = "none";
+  const NoIcon2 = document.querySelector(".third-screen-icon-2");
+  NoIcon2.style.display = "none";
+  const NoIcon3 = document.querySelector(".third-screen-icon-3");
+  NoIcon3.style.display = "none";
+  const NoIcon4 = document.querySelector(".third-screen-icon-4");
+  NoIcon4.style.display = "none";
+};
+
+const iconView = function () {
+  const UpIcon = document.querySelector(".third-screen-icon");
+  UpIcon.style.display = "inline";
+  const UpIcon2 = document.querySelector(".third-screen-icon-2");
+  UpIcon2.style.display = "inline";
+  const UpIcon3 = document.querySelector(".third-screen-icon-3");
+  UpIcon3.style.display = "inline";
+  const UpIcon4 = document.querySelector(".third-screen-icon-4");
+  UpIcon4.style.display = "inline";
 };
