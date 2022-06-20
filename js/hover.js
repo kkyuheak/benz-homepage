20d0218dd593df57f71f5708cbd93cc0473d0c47
gyuheak@gmail.com <gyuheak@gmail.com>
2022-06-21 02:08:04 +0900

    Project

--- js/hover.js
+++ js/hover.js
@@ -1,1 +1,11 @@
+const msOverIcon = document.getElementById("fr-icon_button");
+const msOverText = document.getElementById("fr-hidden-text");
+const backgrOver = document.getElementById("fr-screen");
 
+msOverIcon.addEventListener("mouseover", () => {
+  backgrOver.style.filter = "blur(7px)";
+});
+
+msOverIcon.addEventListener("mouseleave", () => {
+  backgrOver.style.filter = "blur(0px)";
+});
