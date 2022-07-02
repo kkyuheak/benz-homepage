57b957530190130e03c8d8116208e6a8fa0b144f
gyuheak@gmail.com <gyuheak@gmail.com>
2022-07-03 01:17:51 +0900

    Project

---
+++ js/last-screen.js
@@ -0,0 +1,13 @@
+const sideBtn1 = document.getElementById("sideBtn-1");
+const sideBtn2 = document.getElementById("sideBtn-2");
+const sideBtn3 = document.getElementById("sideBtn-3");
+const sideBtn4 = document.getElementById("sideBtn-4");
+const sideBtn5 = document.getElementById("sideBtn-5");
+const sideBtn6 = document.getElementById("sideBtn-6");
+const sideBtn7 = document.getElementById("sideBtn-7");
+const sideBtn8 = document.getElementById("sideBtn-8");
+
+sideBtn1.addEventListener("click", () => {
+  sideBtn1.style.backgroundColor = "black";
+  sideBtn1.style.color = "white";
+});
