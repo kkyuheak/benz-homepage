cfaf4154adeaf64cf808fece86d6ad033bf466e1
gyuheak@gmail.com <gyuheak@gmail.com>
2022-07-04 01:44:55 +0900

    Project

--- js/last-screen.js
+++ js/last-screen.js
@@ -7,7 +7,94 @@ const sideBtn6 = document.getElementById("sideBtn-6");
 const sideBtn7 = document.getElementById("sideBtn-7");
 const sideBtn8 = document.getElementById("sideBtn-8");
 
-sideBtn1.addEventListener("click", () => {
+function Btn1backBlack() {
   sideBtn1.style.backgroundColor = "black";
   sideBtn1.style.color = "white";
+}
+
+function Btn1backWhite() {
+  sideBtn1.style.backgroundColor = "white";
+  sideBtn1.style.color = "black";
+}
+
+function Btn2backBlack() {
+  sideBtn2.style.backgroundColor = "black";
+  sideBtn2.style.color = "white";
+}
+
+function Btn2backWhite() {
+  sideBtn2.style.backgroundColor = "white";
+  sideBtn2.style.color = "black";
+}
+
+function Btn2backBlack() {
+  sideBtn2.style.backgroundColor = "black";
+  sideBtn2.style.color = "white";
+}
+
+function Btn2backWhite() {
+  sideBtn2.style.backgroundColor = "white";
+  sideBtn2.style.color = "black";
+}
+
+function Btn2backBlack() {
+  sideBtn2.style.backgroundColor = "black";
+  sideBtn2.style.color = "white";
+}
+
+function Btn2backWhite() {
+  sideBtn2.style.backgroundColor = "white";
+  sideBtn2.style.color = "black";
+}
+
+function Btn2backBlack() {
+  sideBtn2.style.backgroundColor = "black";
+  sideBtn2.style.color = "white";
+}
+
+function Btn2backWhite() {
+  sideBtn2.style.backgroundColor = "white";
+  sideBtn2.style.color = "black";
+}
+
+function Btn2backBlack() {
+  sideBtn2.style.backgroundColor = "black";
+  sideBtn2.style.color = "white";
+}
+
+function Btn2backWhite() {
+  sideBtn2.style.backgroundColor = "white";
+  sideBtn2.style.color = "black";
+}
+
+function Btn2backBlack() {
+  sideBtn2.style.backgroundColor = "black";
+  sideBtn2.style.color = "white";
+}
+
+function Btn2backWhite() {
+  sideBtn2.style.backgroundColor = "white";
+  sideBtn2.style.color = "black";
+}
+
+function Btn2backBlack() {
+  sideBtn2.style.backgroundColor = "black";
+  sideBtn2.style.color = "white";
+}
+
+function Btn2backWhite() {
+  sideBtn2.style.backgroundColor = "white";
+  sideBtn2.style.color = "black";
+}
+
+sideBtn1.addEventListener("click", () => {
+  Btn1backWhite(sideBtn1);
+  Btn2backBlack(sideBtn2);
+});
+
+sideBtn2.addEventListener("click", () => {
+  Btn1backBlack(sideBtn1);
+
+  sideBtn2.style.backgroundColor = "white";
+  sideBtn2.style.color = "black";
 });
